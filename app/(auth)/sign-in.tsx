b740a71f023e5f1a63fe2527bf476a05a3746512
Md. Salman TD <83644036+MdSalmanTd@users.noreply.github.com>
2026-05-20 03:20:26 +0600

    Merge pull request #1 from MdSalmanTd/dev
    feat: project setup , nativewind, routing, tab navigaion

--- app/(auth)/sign-in.tsx
+++ app/(auth)/sign-in.tsx
@@ -1,14 +1,21 @@
 import { View, Text } from 'react-native'
 import { Link } from "expo-router";
+import { styled } from "nativewind";
+import { SafeAreaView as RNSafeAreaView } from "react-native-safe-area-context";
+
+
+const SafeAreaView = styled(RNSafeAreaView);
+
+
 
 const Signin = () => {
   return (
-    <View>
+    <SafeAreaView className="flex-1 p-5 bg-background">
       <Text>sign-in</Text>
       <Link href="/(auth)/sign-up" className="mt-4 text-blue-500">
-        Don't have an account? Sign Up
+        {"Don't have an account? Sign Up"}
       </Link>
-    </View>
+    </SafeAreaView>
   )
 }
 
